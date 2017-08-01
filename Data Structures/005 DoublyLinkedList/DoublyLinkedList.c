2193bce5304a27ec784f4e297612410496112402
simpletalkcpp <simpletalkcpp@gmail.com>
2017-08-01 05:24:35 +0200

    004 Linked List.sln 005 DoublyLinkedList.sln

---
+++ Data Structures/005 DoublyLinkedList/DoublyLinkedList.c
@@ -0,0 +1,173 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+#include <conio.h>
+#include <stdint.h>
+#include <string.h>
+#include <stdlib.h>
+#include <assert.h>
+
+struct DListNodeStruct {
+	struct DListNodeStruct* prev;
+	struct DListNodeStruct* next;
+};
+
+typedef struct DListNodeStruct DListNode;
+
+struct StudentStruct {
+	DListNode list_node; //common header
+	int id;
+	char name[100];
+	int age;
+	char address[2000];
+};
+
+struct TeachStruct {
+	DListNode list_node; //common header
+
+	int id;
+	char name[100];
+};
+
+typedef struct StudentStruct Student;
+typedef struct TeacherStruct Teacher;
+
+void Student_Init(Student* s) {
+	s->id = 0;
+	s->name[0] = 0;
+	s->age = 0;
+	s->address[0] = 0;
+	s->list_node.prev = NULL;
+	s->list_node.next = NULL;
+}
+
+void Student_InitWithId(Student* s, int id, char* name) {
+	Student_Init(s);
+	s->id = id;
+	strcpy(s->name, name);
+}
+
+Student* Student_NewWithId(int id, char* name) {
+	Student* s = (Student*)malloc(sizeof(Student));
+	Student_InitWithId(s, id, name);
+	return s;
+}
+
+typedef struct {
+	DListNode* head;
+	DListNode* tail;
+	int count;
+} DList;
+
+
+void DList_Init(DList* ls) {
+	ls->head = NULL;
+	ls->tail = NULL;
+	ls->count = 0;
+}
+
+void DList_InsertToHead(DList* ls, DListNode* s) {
+	assert(!s->prev && !s->next);
+
+	s->prev = NULL;
+	s->next = ls->head; //1
+
+	if (s->next) {
+		s->next->prev = s;
+	}
+
+	ls->head = s;
+	ls->count++;
+}
+
+void DList_Insert(DList* ls, DListNode* s, DListNode* after) {
+	assert(!s->prev && !s->next);
+
+	if (!after) {
+		DList_InsertToHead(ls, s);
+		return;
+	}
+
+	s->prev = after; //1
+	s->next = after->next; //2
+
+	if (after->next) {
+		after->next->prev = s; //3
+	} else {
+		ls->tail = s; //3
+	}
+
+	after->next = s; //4
+}
+
+void DList_AppendToTail(DList* ls, DListNode* s);
+void DList_Append(DList* ls, DListNode* s, DListNode* before);
+
+void DList_Release(DList* ls) {
+	DListNode* next;
+	for (DListNode* p = ls->head; p; p = next) {
+		next = p->next;
+		free(p);
+	}
+}
+
+typedef DList StudentList;
+typedef DList ContactList;
+
+void ContactList_InsertStudentToHead(ContactList* ls, Student* s) {
+	DList_InsertToHead((DList*)ls, (DListNode*)s);
+}
+
+void ContactList_InsertTeacherToHead(ContactList* ls, Teacher* s) {
+	DList_InsertToHead((DList*)ls, (DListNode*)s);
+}
+
+void StudentList_Insert(StudentList* ls, Student* s, Student* after) {
+	DList_Insert((DList*)ls, (DListNode*)s, (DListNode*)after);
+}
+
+void StudentList_Init(StudentList* ls) {
+	DList_Init((DList*)ls);
+}
+
+void StudentList_Release(StudentList* ls) {
+	DList_Release((DList*)ls);
+}
+
+void StudentList_Print(DList* ls) {
+	printf("\nprint list count=%d\n", ls->count);
+	for (DListNode* p = ls->head; p; p = p->next) {
+		Student* s = (Student*)p;
+		printf("  %d: %s\n", s->id, s->name);
+	}
+	printf("\n");
+}
+
+
+int main() {
+	StudentList ls;
+	StudentList_Init(&ls);
+
+	Student* john = Student_NewWithId(10, "John");
+	StudentList_Insert(&ls, john, NULL);
+
+	Student* mary = Student_NewWithId(44, "Mary");
+	StudentList_Insert(&ls, mary, NULL);
+
+	Student* tom = Student_NewWithId(6, "Tom");
+	StudentList_Insert(&ls, tom, NULL);
+
+	StudentList_Print(&ls);
+
+	Student* bob = Student_NewWithId(36, "Bob");
+	StudentList_Insert(&ls, bob, john);
+
+	StudentList_Print(&ls);
+
+	StudentList_Release(&ls);
+
+	printf("=== Program Ended ===\n");
+	printf("Press any to key to Exit !");
+	_getch();
+	return 0;
+}
