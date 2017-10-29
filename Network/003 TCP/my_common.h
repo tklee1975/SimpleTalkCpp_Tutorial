a26ba635ddd95bb4514a3aa779b2c9823f1ab4c7
Jason Chan <jasonchan35@gmail.com>
2017-10-29 23:00:33 +0100

    Network 002, 003

---
+++ Network/003 TCP/my_common.h
@@ -0,0 +1,43 @@
+#pragma once
+
+#ifdef _WIN32
+	#include <Winsock2.h> // must include before windows.h
+	#include <Ws2tcpip.h>
+	#include <Windows.h>
+	#include <conio.h>
+	#pragma comment(lib, "Ws2_32.lib")
+#else
+	#include <sys/socket.h>
+	#include <netdb.h>
+	#include <sys/ioctl.h>
+#endif
+
+#include <stdint.h>
+#include <string>
+#include <exception>
+#include <iostream>
+#include <vector>
+#include <stdio.h>
+
+class MyNonCopyable {
+public:
+	MyNonCopyable() = default;
+private:
+	MyNonCopyable(const MyNonCopyable&) = delete;
+	void operator=(const MyNonCopyable&) = delete;
+};
+
+class MyError : public std::exception {
+public:
+	MyError(const char* msg = "") {
+		printf("MyError(%s)\n", msg);
+	}
+};
+
+inline void my_sleep(int sec) {
+#ifdef _WIN32
+	Sleep(sec * 1000);
+#else
+	sleep(src);
+#endif
+}
