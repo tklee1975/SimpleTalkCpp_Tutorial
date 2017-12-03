57960e04f3b64d9c0b39a27321f369a420404e4b
Jason Chan <jasonchan35@gmail.com>
2017-12-03 05:39:39 +0100

    Unity Shader 004, 005, 006

---
+++ Graphic/UnityShader/UnityProject/Assets/Scripts/MyLight.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class MyLight : MonoBehaviour {
+	void Update () {
+
+		var p = gameObject.GetComponent<Light>();
+		if (p == null)
+			return;
+		Shader.SetGlobalVector("MyLightPos", p.transform.position);
+	}
+}
