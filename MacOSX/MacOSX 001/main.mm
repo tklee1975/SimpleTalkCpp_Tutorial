16d4af8377a4a7c8c84178771c1b3f12e63e1f66
Jason <jason@Jasons-Mac.local>
2017-12-17 21:19:49 +0100

    MacOSX 001

---
+++ MacOSX/MacOSX 001/main.mm
@@ -0,0 +1,12 @@
+#include <iostream>
+#include "TestClass.h"
+
+int main(int argc, const char * argv[]) {
+	// insert code here...
+	std::cout << "Hello, World!\n";
+	
+	TestClass c;
+	c.testPrint();
+	
+	return 0;
+}
