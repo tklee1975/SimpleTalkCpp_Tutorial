f44fb7db5d408f2bfff3920b6b201cef9b19e700
simpletalkcpp <simpletalkcpp@gmail.com>
2018-07-30 05:20:01 +0200

    x86 Asm Lesson03

--- Assembly Language/x86 Basic/Lesson_03/Ex003/MyAsm.asm
+++ Assembly Language/x86 Basic/Lesson_03/Ex003/MyAsm.asm
@@ -1,22 +1,24 @@
 .686
 .XMM
 
-.model flat, c
+.model flat, stdcall
 
 option casemap :none
 
 .code
 
 asm_add_cpp PROC arg_dst:DWORD, arg_a:DWORD, arg_b:DWORD, arg_size:DWORD
+	pusha
+
 	mov ecx, arg_size
 	mov esi, arg_a
 	mov ebx, arg_b
 	mov edi, arg_dst
 
 my_loop:
-	movss xmm0, DWORD PTR[esi]
-	addss xmm0, DWORD PTR[ebx]
-	movss DWORD PTR[edi], xmm0
+	movaps xmm0, DWORD PTR[esi]
+	addps  xmm0, DWORD PTR[ebx]
+	movaps DWORD PTR[edi], xmm0
 
 	add esi, 16;
 	add ebx, 16;
@@ -25,6 +27,7 @@ my_loop:
 	dec ecx
 	jnz my_loop
 
+	popa
 	ret
 asm_add_cpp ENDP
 
