8360f78705183eba6b4da3e8f69d5016064bdb7b
SimpleTalkCpp <simpletalkcpp@gmail.com>
2021-02-24 04:27:02 +0100

    Unity Basic #014

---
+++ Unity/Unity Basic/Assets/Lesson 014/Lesson014_TestPanel.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Lesson014_TestPanel : MonoBehaviour
+{
+	public Dropdown testDropdown;
+
+	private void Start()
+	{
+		if (!testDropdown) {
+			Debug.LogError("missing testDropdown");
+		} else {
+			testDropdown.AddOptions(new List<string>{"Test1", "Test2"});
+
+			testDropdown.onValueChanged.AddListener(OnDropdownChanged);
+		}
+	}
+
+	public void OnTestButtonClick() {
+		Debug.Log("OnTestButtonClick");
+	}
+
+	public void OnDropdownChanged(int v) {
+		Debug.Log($"OnDropdownChanged {v}");
+	}
+
+}
