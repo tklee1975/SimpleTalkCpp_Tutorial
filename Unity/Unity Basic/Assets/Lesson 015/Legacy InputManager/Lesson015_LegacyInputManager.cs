d60c4905eb33f1651eb360a98a2f9c932330c2cf
SimpleTalkCpp <simpletalkcpp@gmail.com>
2021-02-28 08:03:43 +0100

    Unity Basic 015

---
+++ Unity/Unity Basic/Assets/Lesson 015/Legacy InputManager/Lesson015_LegacyInputManager.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lesson015_LegacyInputManager : MonoBehaviour
+{
+	private void OnGUI()
+	{
+
+        GUI.Box( new Rect(10, 10, 300, 200),
+				"== Lesson015_LegacyInputManager ==\n"
+				+ $"MousePos = {Input.mousePosition}\n"
+				+ $"Vertical = {Input.GetAxis("Vertical")}\n"
+				+ $"Fire1 = {Input.GetAxis("Fire1")}\n"
+				+ $"Fire1 Button = {Input.GetButton("Fire1")}");
+    }
+}
